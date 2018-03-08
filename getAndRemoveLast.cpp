0a74ca7f03ad5682fb1ff3a3b156c421c3206fb1
Yoko <1033034935@qq.com>
2018-03-08 22:03:23 +0800

    upgrade

---
+++ getAndRemoveLast.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int getAndRemove(stack<int>& s)
+{
+	int result = s.top();
+	s.pop();
+	if (s.empty())
+		return result;
+	else
+	{
+		int last = getAndRemove(s);
+	    s.push(result);
+		return last;
+	}
+}
+void reverse(stack<int>& s)
+{
+	if (s.empty())
+		return;
+	int i = getAndRemove(s);
+	reverse(s);
+	s.push(i);
+}
+void show(stack<int> s)
+{
+	while (!s.empty())
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+	cout << endl;
+}
+int main()
+{
+	stack <int>stk;
+	//»Î’ª
+	stk.push(1);
+	stk.push(2);
+	stk.push(3);
+	cout << "before:";
+	show(stk);
+	reverse(stk);
+	cout << "after:";
+	show(stk);
+	system("pause");
+	return 0;
+}
---
+++ push.sh
@@ -0,0 +1,4 @@
+git add
+git add .
+git commit -m"upgrade"
+git push origin1 master
